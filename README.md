5159119b5694008b90ca745dbe39294653faa849
Cloudiees <73859729+cloudiees@users.noreply.github.com>
2025-08-29 23:17:43 -0500

    minor tweaks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### TODO List:
 - All page content
 - Maybe add something animated to the home page, looks kind of dry right now
+- Add more content to footer
+- Maybe add some sort of contact me thing
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,14 @@ import GithubLogo from "../assets/github-mark-white.png"
 export default function Footer() {
     return(
         <div className="footer">
-            <div className="footer-component">
-                <a href="https://github.com/cloudiees" target="_blank"><img src={GithubLogo} className="footer-img"></img></a>
-                <a href="https://github.com/cloudiees" target="_blank" className="footer-link">Github</a>
+            <div className="footer-vertical">
+                <div className="footer-component">
+                    <a href="https://github.com/cloudiees" target="_blank"><img src={GithubLogo} className="footer-img"></img></a>
+                    <a href="https://github.com/cloudiees" target="_blank" className="footer-link">Github</a>
+                </div>
+                <div>
+                    <text>© 2025 Cloudiees</text>
+                </div>
             </div>
         </div>
     )
