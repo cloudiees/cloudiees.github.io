763623b8644d70819c30985e6d4fdcdba53dd875
Ibrahim <73859729+cloudiees@users.noreply.github.com>
2025-09-23 10:11:33 -0500

    Update About.jsx

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
                 <span>
                     Hello, I'm Ibrahim! I'm currently pursuing my bachelor's degree in Computer Science at the University of Illinois Chicago, with plans to continue on to a master's in the near future.
                     <br /><br />
-                    I have a passion for creating and building, whether that means coding projects, designing custom keyboards, or exploring new ideas through hands-on learning. In my free time, I also enjoy gaming and diving into new technologies.
+                    I have a passion for creating and building, whether that means coding projects, building custom keyboards, or exploring new ideas through hands-on learning. In my free time, I also enjoy gaming and diving into new technologies.
                 </span>
             </div>
 
